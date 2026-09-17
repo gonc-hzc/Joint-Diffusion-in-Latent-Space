e126b941c0470e99395f1df4debfff593899a508
gonc <hzhicong126@gmail.com>
2026-09-17 21:11:52 +0800

    在训练脚本中添加批量大小和梯度累积批次的命令行参数；更新配置以支持这些参数

--- callbacks.py
+++ callbacks.py
@@ -531,15 +531,21 @@ class CheckpointEveryNSteps(pl.Callback):
         self.save_step_frequency = save_step_frequency
         self.prefix = prefix
         self.use_modelcheckpoint_filename = use_modelcheckpoint_filename
+        self.last_global_step_saved = None
 
     def on_train_batch_end(self, trainer, pl_module, outputs, batch, batch_idx):
         """Check if we should save a checkpoint after every train batch"""
         epoch = trainer.current_epoch
         global_step = trainer.global_step
-        if global_step % self.save_step_frequency == 0 and global_step > 0:
+        if (
+            global_step % self.save_step_frequency == 0
+            and global_step > 0
+            and global_step != self.last_global_step_saved
+        ):
             if self.use_modelcheckpoint_filename:
                 filename = trainer.checkpoint_callback.filename
             else:
                 filename = f"{self.prefix}_{epoch=}_{global_step=}.ckpt"
             ckpt_path = os.path.join(trainer.checkpoint_callback.dirpath, filename)
             trainer.save_checkpoint(ckpt_path)
+            self.last_global_step_saved = global_step
